3765744a02a5695b181d15bc2b7ae6f57c939436
Piotr Czajka <czajka@protonmail.com>
2018-02-20 14:06:31 +0100

    First working version of GUI proximity sensor done

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+#!/usr/bin/env python3
+
 from kivy.app import App
+from kivy.uix.progressbar import ProgressBar
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import StringProperty, NumericProperty
+
+PINK = 1, 0, 1, 1
+BLACK = 0, 0, 0, 1
+WHITE = 1, 1, 1, 1
 
 
 class Sensor(FloatLayout):
     text = StringProperty('')
-    reading = NumericProperty(0)
+    reading = NumericProperty(5)
 
 
 class ProximitySensor(Sensor):
-    percentage = NumericProperty(0.5)
+    max = NumericProperty(10)
+    min = NumericProperty(0)
+
+
+class ZipIndicator(ProgressBar):
+    pass
 
 
 class Gyroscope(Sensor):
