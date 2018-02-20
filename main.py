50ec44d96263522a22af93678db43d4bcda352e8
Piotr Czajka <czajka@protonmail.com>
2018-02-20 15:24:56 +0100

    First working version of potentiometer GUI

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 #!/usr/bin/env python3
+from functools import partial
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty, NumericProperty, ListProperty
+from kivy.clock import Clock
 
 PINK = 1, 0, 1, 1
 BLACK = 0, 0, 0, 1
 WHITE = 1, 1, 1, 1
 
 
+def some_function(obj, *args, **kwargs):
+    obj.reading = 200
+    print('dupsko')
+
+
 class Sensor(FloatLayout):
     text = StringProperty('')
-    reading = NumericProperty(5)
+    reading = NumericProperty(20)
+    color = ListProperty([1, 1, 1, 1])
 
 
 class ProximitySensor(Sensor):
-    max = NumericProperty(10)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    max = NumericProperty(1000)
     min = NumericProperty(0)
 
 
 class ZipIndicator(ProgressBar):
     pass
+
+
+class CircleIndicator(Widget):
+    percentage = NumericProperty(0)
+    max = NumericProperty(270)
+    min = NumericProperty(0)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class Gyroscope(Sensor):
